5c61ae632d52053f8880c7febac0071d5be70011
przemyslawkulak <przemyslaw.kulak86@gmail.com>
2019-02-05 20:02:08 +0100

    creating models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Institution(models.Model):
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+
+
+class Gift(models.Model):
+    name = models.CharField(max_length=255)
+    date = models.DateTimeField(null=True)
+    given = models.BooleanField(default=False)
+    institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
