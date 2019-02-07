6757716ad80d74735d6ccff921417231ab50d1a9
przemyslawkulak <przemyslaw.kulak86@gmail.com>
2019-02-07 14:19:35 +0100

    logout

--- Oddaj_rzeczy/urls.py
+++ Oddaj_rzeczy/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from app.views import LandingPageView, LoginView
+from app.views import LandingPageView, LoginView, LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', LandingPageView.as_view(), name='landing-page'),
     path('accounts/login/', LoginView.as_view(), name="login"),
+    path('logout/', LogoutView.as_view(), name='logout')
 ]
