fbe7c146c2d6545fbbb8f68b9849644c797aff71
przemyslawkulak <przemyslaw.kulak86@gmail.com>
2019-02-08 16:23:15 +0100

    update profile update password

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
@@ -84,3 +84,51 @@ class RegisterView(View):
                 return redirect('login')
         text = 'Żle powtórzone hasło'
         return render(request, 'app/register.html', {"text": text})
+
+
+class ProfileView(View):
+    def get(self, request):
+        return render(request, 'app/profile.html')
+
+    def post(self, request):
+        username = request.POST.get("login")
+        email = request.POST.get('email')
+        name = request.POST.get('name')
+        surname = request.POST.get('surname')
+        password = request.POST.get('password')
+        confirmPassword = request.POST.get('password2')
+        u = User.objects.get(username=request.user)
+        users = User.objects.all()
+        usernames = []
+        emails = []
+        for i in users:
+            emails.append(i.email)
+            usernames.append(i.username)
+
+        if username and email and name and surname:
+            if username in usernames and username != request.user.username:  # sprawdza czy nie ma innych userów o tej nazwie
+                text = 'Podany user już istnieje'
+                return render(request, 'app/profile.html', {"text": text})
+            elif email in emails and email != request.user.email:
+                text = 'Podany email już istnieje'
+                return render(request, 'app/profile.html', {"text": text})
+            else:
+                u.username = username
+                u.email = email
+                u.first_name = name
+                u.last_name = surname
+                u.save()
+                text = 'Udana edycja danych'
+                return render(request, 'app/profile.html', {"text": text})
+
+        if password and confirmPassword and password == confirmPassword:
+            if u.check_password(request.POST['pastpassword']):  # sprawdza stare hasło
+                u.set_password(password)  # zmienia hasło na nowe
+                update_session_auth_hash(request, u)  # nie wylogowuje po aktualizacji hasła
+                u.save()
+                text = 'Udana aktualizacja hasła'
+                return render(request, 'app/profile.html', {"text": text})
+            text = 'Nieprawidłowe stare hasło'
+            return render(request, 'app/profile.html', {"text": text})
+        text = 'Żle powtórzone hasło'
+        return render(request, 'app/profile.html', {"text": text})
