14223aa1ce6335ac36766c4c41e186be4b860b60
przemyslawkulak <przemyslaw.kulak86@gmail.com>
2019-04-15 11:27:50 +0200

    deploying update

--- Oddaj_rzeczy/settings.py
+++ Oddaj_rzeczy/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost', 'oddaj-rzeczy.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', 'oddaj-rzeczy.herokuapp.com']
 
 # Application definition
 
@@ -138,7 +138,7 @@ except ModuleNotFoundError:
         }
     }
 
-    DEBUG = False
+    DEBUG = True
 
     SECRET_KEY = os.environ.get('SECRET_KEY', 'default')
 
