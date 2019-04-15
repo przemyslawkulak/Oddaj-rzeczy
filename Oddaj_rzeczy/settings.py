1aa03ec1370acbcc8195324a2cc6ef37915e7946
przemyslawkulak <przemyslaw.kulak86@gmail.com>
2019-04-15 12:23:34 +0200

    deploying update database

--- Oddaj_rzeczy/settings.py
+++ Oddaj_rzeczy/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-import DATABASES as DATABASES
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
